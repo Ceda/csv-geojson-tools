bf8a594ddf0d5295ef7dc0d3c9742631918e48ac
Tonda Pleskac <pleskac@gmail.com>
2019-04-17 16:23:15 +0200

    Init

--- index.js
+++ index.js
@@ -7,8 +7,48 @@ const buffer = fs.readFileSync(file);
 
 const content = JSON.parse(buffer);
 
-const geojsonToCsv = require(path.resolve(__dirname, "./to-csv.js"));
+/// Read csv
+const csvFile = path.resolve("csv/test.csv");
 
-const csvString = geojsonToCsv(content, ";");
+const csvBuffer = fs.readFileSync(csvFile);
 
-console.log(csvString);
+const csvContent = csvBuffer.toString();
+
+var csv2geojson = require("csv2geojson");
+
+// csv2geojson.csv2geojson(csvContent, function(err, data) {
+//   console.log(data);
+// });
+
+const geoJsonFromCsv = csv2geojson.csv2geojson(csvContent, function(err, data) {
+  // err has any parsing errors
+  // data is the data.
+  console.log(data.features);
+});
+
+// const geoJsonFromCsv = csv2geojson.auto(csvContent);
+// console.log(geoJsonFromCsv);
+
+// // Json to CSV
+// const converter = require("json-2-csv");
+//
+// converter
+//   .json2csvAsync(content, { delimiter: { field: ";" } })
+//   .then(response => {
+//     console.log(response);
+//   });
+
+// // Geo Json validator
+// const validator = require("geojson-validation");
+//
+// validator.valid(content, function(valid, errs) {
+//   console.log(valid);
+//   console.log(errs);
+// });
+
+// // Convert geojson to csv
+// const geojsonToCsv = require(path.resolve(__dirname, "./to-csv.js"));
+//
+// const csvString = geojsonToCsv(content, ";");
+//
+// console.log(csvString);
